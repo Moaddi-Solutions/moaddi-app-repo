9a25ddffa3d8466db293b20012acca0b2e1f015d
Moaddi <174136284+moaddi90@users.noreply.github.com>
2026-08-11 08:59:48 +0300

    hardcode updates config for EAS CLI detection

--- vending_app/app.config.js
+++ vending_app/app.config.js
@@ -21,7 +21,12 @@ const staticOrigin = normalizeOrigin(
 module.exports = {
   expo: {
     ...appJson.expo,
-    updates: appJson.expo.updates,
+    updates: {
+      url: "https://u.expo.dev/047135ad-2d67-441c-a36c-f56ebce0a5fe",
+      enabled: true,
+      checkAutomatically: "ON_LOAD",
+      fallbackToCacheTimeout: 0,
+    },
     extra: {
       ...appJson.expo.extra,
       serverOrigin,
